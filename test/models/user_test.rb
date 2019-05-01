19b2ee6f028fef694b563ad291003a1db96a757b
Christopher Lutz <chrisblutz@frontier.com>
2019-05-01 11:31:51 -0400

    Fix styling

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,13 +5,13 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   test 'valid' do
     user = User.create(name: 'Example User', email: 'user@example.com', \
-                    password: 'password', password_confirmation: 'password')
+                       password: 'password', password_confirmation: 'password')
     assert user.valid?
     user = User.create(name: 'Example User', \
-                    password: 'password', password_confirmation: 'password')
+                       password: 'password', password_confirmation: 'password')
     assert_not user.valid?
     user = User.create(name: 'Example User', email: 'user@example.com', \
-                      password: 'password', password_confirmation: 'password')
+                       password: 'password', password_confirmation: 'password')
     assert_not user.valid?
   end
 
@@ -24,7 +24,7 @@ class UserTest < ActiveSupport::TestCase
 
   test 'email valid' do
     user = User.create(name: 'Example User', email: 'userexample.com', \
-                      password: 'password', password_confirmation: 'password')
+                       password: 'password', password_confirmation: 'password')
     assert_not user.valid?
   end
 end
