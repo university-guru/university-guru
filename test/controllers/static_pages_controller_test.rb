f7d78b5d0ccab319c425f2ae57d067f767f723fe
Christopher Lutz <chrisblutz@frontier.com>
2019-05-06 10:11:54 -0400

    Remove auto-generated tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,13 +3,4 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test 'should get moderator' do
-    get static_pages_moderator_url
-    assert_response :success
-  end
-
-  test 'should get administrator' do
-    get static_pages_administrator_url
-    assert_response :success
-  end
 end
